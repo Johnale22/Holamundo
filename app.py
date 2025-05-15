19a685d20b807249fb4b282065ce508f7eb6eeb1
JohnAle2207 <jaguerrac@uce.edu.ec>
2025-05-15 01:35:30 -0500

    first commit5

--- app.py
+++ app.py
@@ -9,5 +9,5 @@ def home():
 
 # Este bloque solo se ejecuta si corres python app.py localmente
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))  # DigitalOcean usará PORT, local será 5000
-    app.run(host='0.0.0.0', port=port, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
