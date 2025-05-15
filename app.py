88a54c2c76922bcbb3f55cb0c121f576aeb0b7cd
JohnAle2207 <jaguerrac@uce.edu.ec>
2025-05-15 01:28:23 -0500

    first commit3

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 import os
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static', template_folder='templates')
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
+# Este bloque solo se ejecuta si corres python app.py localmente
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    port = int(os.environ.get("PORT", 5000))  # DigitalOcean usará PORT, local será 5000
+    app.run(host='0.0.0.0', port=port, debug=True)
